611c91aec628bd409e53818ca6b773410dd078d7
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-12-07 15:04:23 -0800

    Update universal.js

--- JS/universal.js
+++ JS/universal.js
@@ -1,9 +1,9 @@
 //Ban Devices
-var bannedDeviceIDS = ["X11; CrOS x86_64 14588.98.0"];
+var bannedDeviceIDS = ["X11; CrOS aarch64 15117.111.0"];
 var deviceId = navigator.appVersion.split('(')[1].split(')')[0];
-  if (bannedDeviceIDS.indexOf(deviceId) != -1) {
-    window.location = "/banned.html"
-  }
+if (bannedDeviceIDS.indexOf(deviceId) !== -1) {
+  window.location = "/banned.html"
+}
 
 
 //Custom Alert Boxes
