7989d9a6094ff43aa15c5e121d55fb8492d7149f
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-12-12 22:44:32 -0800

    Add files via upload

--- JS/bannedEmails.js
+++ JS/bannedEmails.js
@@ -1,1 +1,14 @@
-export var bannedEmails = ["nathanaschilling@gvusd.org"]
+function bannedEmail() {
+  var bannedEmails = ["nathanaschilling@gvusd.org"]
+  if (bannedEmails.indexOf(useremail) != -1) {
+            var redirectURL = (new window.URL(window.location.href)).searchParams.get('redirect');
+            window.location = "banned.html?redirect="+redirectURL;
+         } else {
+            var redirectURL = (new window.URL(window.location.href)).searchParams.get('redirect');
+            if (isValidUrl(redirectURL)) {
+              window.location = redirectURL
+            } else {
+              window.location = "index.html"
+            };
+         }
+}
