f1567286ab9301903653e39bce9dc79f201f7f01
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-12-13 22:17:31 -0800

    Update universal.js

--- JS/universal.js
+++ JS/universal.js
@@ -111,35 +111,14 @@ if (urlparams != "no") {
     }
   }
 }
-
-//Url Hider / About:blank Tab
-if (localStorage.getItem('UrlHiderEnabled') == null) {localStorage.setItem('UrlHiderEnabled', 'false')}
-if (localStorage.getItem('UrlHiderEnabled') == "true") {
-  var win = null;
-      if (parent.window.location.href != 'about:blank') {
-        if (popUps == "yes") {
-          win = window.open('about:blank', 'win', 'top=0,left=0,scrollbar=yes,width=10000,height=10000');
-          win.focus();
-        }
-        
-        win.document.body.style.margin = "0";
-        win.document.body.style.height = "100vh";
-        var iframe = win.document.createElement("iframe");
-        iframe.style.border = "none";
-        iframe.style.width = "100%";
-        iframe.style.height = "100%";
-        iframe.style.margin = "0";
-        iframe.src = window.location.href;
-        var title = win.document.createElement("title");
-        title.innerHTML = document.title;
-        win.document.head.appendChild(title);
-        win.document.body.appendChild(iframe);
-        
+window.addEventListener('keydown', event => {
+    if (event.keycode == '77') {
         var url = ["https://www.google.com/", "https://classroom.google.com/", "https://drive.google.com/", "https://mail.google.com/", "https://www.youtube.com/", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"];
         
         window.location.href = url[Math.floor(Math.random() * url.length)];
-        }
-}
+    }
+});        
+
 
 //Misc 
 function openGame(url, title) {
@@ -168,12 +147,7 @@ function bannedEmail(email) {
             if (window.location.href.includes('banned') == false) {
               window.location = "./banned.html?redirect="+redirectURL;
             }
-         } else {
-            var redirectURL = (new window.URL(window.location.href)).searchParams.get('redirect');
-            if (redirectURL == null || redirectURL == undefined) {
-              window.location = './index.html'
-            } else {window.location = redirectURL}
-         }
+  }
 }
 
 if (localStorage.getItem("userEmail") != '') {
